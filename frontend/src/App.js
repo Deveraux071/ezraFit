044ff2eee8383f02860d098a0b0685d7377819c0
abhigyachhabra <abhigya.chhabra@mail.utoronto.ca>
2023-02-08 14:19:47 -0500

    updated landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Insert frontend here</p>
+        <p>EzraFit</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
