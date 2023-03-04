c7d49ae7ef55799e34894d3c85a47c2883de893c
Aashna Vasudeva <aashna.vasudeva@mail.utoronto.ca>
2023-03-04 18:14:42 -0500

    sentry connected to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://fa4388015e9c4b46bf10c8e7ab0ab7fd@o358880.ingest.sentry.io/4504776458698752",
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 0.2,
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
