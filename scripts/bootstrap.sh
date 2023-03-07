8e382ad366041c51094f915bbcfd7b5a6f457c2f
Anjali11 <anjali.parikh@mail.utoronto.ca>
2023-03-06 20:06:23 -0500

    adding scripts to add requriements;

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,19 @@
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Hombrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
+echo "Installing python3"
+brew install python3
+
+
+cd frontend
+npm install
+cd -
+cd backend
+python3 -m venv be
+source be/bin/activate
+pip install -r requirements.txt
