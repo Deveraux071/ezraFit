1d32120b1b225087c9c9ed0e20618ff155e95e4e
Aashna Vasudeva <aashna.vasudeva@mail.utoronto.ca>
2023-03-31 16:39:30 -0400

    failing ci fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { TakeImage } from './camera-pages/take-image';
 import { ViewImage } from './camera-pages/view-image';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Test from './test';
 import Calculating from './camera-pages/calculating';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { theme } from './theme';
@@ -20,7 +19,6 @@ function App() {
         <Routes>
           <Route path='/take-image' element={<TakeImage/>}/>
           <Route path='/view-image' element={<ViewImage/>}/>
-          <Route path='/test' element={<Test/>}/>
           <Route path='/calculating' element={<Calculating/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,15 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
+import { initializeApp } from "firebase/app";
 
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyAjCdDCnmWx2NWwSLDFeHcD1jLem4jV1fU",
-    authDomain: "ezrafit-e157e.firebaseapp.com",
-    databaseURL: "https://ezrafit-e157e-default-rtdb.firebaseio.com",
-    projectId: "ezrafit-e157e",
-    storageBucket: "ezrafit-e157e.appspot.com",
-    messagingSenderId: "940654216053",
-    appId: "1:940654216053:web:b0d874482b3d3a186bc93c",
-    measurementId: "G-YSXP4J9PSW"
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyAjCdDCnmWx2NWwSLDFeHcD1jLem4jV1fU",
+  authDomain: "ezrafit-e157e.firebaseapp.com",
+  databaseURL: "https://ezrafit-e157e-default-rtdb.firebaseio.com",
+  projectId: "ezrafit-e157e",
+  storageBucket: "ezrafit-e157e.appspot.com",
+  messagingSenderId: "940654216053",
+  appId: "1:940654216053:web:b0d874482b3d3a186bc93c",
+  measurementId: "G-YSXP4J9PSW"
+};
 
-export const auth = app.auth()
-export default app
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
