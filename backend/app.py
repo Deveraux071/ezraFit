1d604f2220f9f83fb3fd6e4343d5627d3266903e
abhigyachhabra <abhigya.chhabra@mail.utoronto.ca>
2023-04-05 20:20:27 -0400

    Adding API endpoints for training and predictions

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ import sentry_sdk
 from flask import Flask
 from sentry_sdk.integrations.flask import FlaskIntegration
 from firebase import firebase
+from style_recommender import train_classifier
 
 firebase = firebase.FirebaseApplication('https://ezrafit-e157e-default-rtdb.firebaseio.com/', None)
 
@@ -27,6 +28,59 @@ def hello():
 @app.route('/debug-sentry')
 def trigger_error():
   return 1/0
+
+@app.post('/train_usage')
+def train_usage():
+  global usage_model
+  global usage_classes
+  usage_res = train_classifier.train_usage_model()
+  usage_model = usage_res[0]
+  usage_classes = usage_res[1]
+
+  return "ok"
+
+@app.post('/train_all_models')
+def train_models():
+  global usage_model
+  global usage_classes
+  global article_model
+  global article_classes
+  global season_model
+  global season_classes
+  global bc_model
+  global bc_classes
+  
+  usage_res = train_classifier.train_usage_model()
+  usage_model = usage_res[0]
+  usage_classes = usage_res[1]
+
+  article_res = train_classifier.train_article_model()
+  article_model = article_res[0]
+  article_classes = article_res[1]
+
+  season_res = train_classifier.train_season_model()
+  season_model = season_res[0]
+  season_classes = season_res[1]
+
+  bc_res = train_classifier.train_bc_model()
+  bc_model = bc_res[0]
+  bc_classes = bc_res[1]
+
+  return "ok"
+
+@app.get('/predict_usage')
+def predict_usage():
+  return train_classifier.predict(usage_model, usage_classes, "backend/demo_data/1855.jpg")
+
+@app.get('/predict_all')
+def predict_all():
+  usage = train_classifier.predict(usage_model, usage_classes, "backend/demo_data/1855.jpg")
+  article = train_classifier.predict(article_model, article_classes, "backend/demo_data/1855.jpg")
+  season = train_classifier.predict(season_model, season_classes, "backend/demo_data/1855.jpg")
+  bc = train_classifier.predict(bc_model, bc_classes, "backend/demo_data/1855.jpg")
+
+  to_return = [article, usage, season, bc]
+  return to_return
 
 if __name__ == "__main__":
   app.run()
