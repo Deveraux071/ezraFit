160d236b7e384c9700fe958d606c5c89b41099da
Anjali5122 <47335565+Anjali5122@users.noreply.github.com>
2023-04-13 00:24:07 -0400

    Update test_measurement.py

--- backend/tests/test_measurement.py
+++ backend/tests/test_measurement.py
@@ -22,7 +22,7 @@ class TestUtils(unittest.TestCase):
     
     def test_analyze_chessboard_affline_false(self):
         # takes in image 1 , affline flag
-        image1 = cv2.imread('./body_measurement/test_images/final_saket1.jpg')
+        image1 = cv2.imread('/backend/body_measurement/test_images/final_saket1.jpg')
         metre_pixel_x,metre_pixel_y,coordinate,affine_correct_parameters = analyze_chessboard(image1, 'False')
         metre_pixel_x_act = 0.04431528330296793 
         metre_pixel_y_act = 0.04419255298436231
@@ -37,7 +37,7 @@ class TestUtils(unittest.TestCase):
     # test affline_correct and also affine_correct_params
     def test_affine_correct_M_None(self):
         # takes in image 1 , affline flag
-        image1 = cv2.imread('./body_measurement/test_images/final_saket1.jpg')
+        image1 = cv2.imread('./backend/body_measurement/test_images/final_saket1.jpg')
         dst_out = affine_correct(image1)
         dst_act_shape = (2268, 4032, 3)
         self.assertEqual(dst_out.shape, dst_act_shape)
@@ -45,7 +45,7 @@ class TestUtils(unittest.TestCase):
 
     # test affline_correct and also affine_correct_params
     def test_affine_correct_M_not_None(self):
-        image2 = cv2.imread('./body_measurement/test_images/final_saket2.jpg')
+        image2 = cv2.imread('./backend/body_measurement/test_images/final_saket2.jpg')
         affine_correct_parameters_act_true = np.array([[9.40904323e-01, -2.06049356e-02, 8.65506247e+01], [6.93225464e-03, 9.72540133e-01, -8.98750876e+00], [-1.60726699e-05, 2.47661411e-06, 1.00000000e+00]])
         dst_out = affine_correct(image2, affine_correct_parameters_act_true)
         dst_act_shape = (2268, 4032, 3)
@@ -53,7 +53,7 @@ class TestUtils(unittest.TestCase):
         self.assertTrue(isinstance(dst_out, (np.ndarray, np.generic) ))
 
     def test_chess_board(self):
-        image1 = cv2.imread('./body_measurement/test_images/final_saket1.jpg')
+        image1 = cv2.imread('./backend/body_measurement/test_images/final_saket1.jpg')
         gray=np.copy(image1)
         gray=cv2.cvtColor(gray,cv2.COLOR_BGR2GRAY)
         coordinates_out = chess_board_corners(image1, gray, 5)
