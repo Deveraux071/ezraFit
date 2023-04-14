30958e493e578da233edcfef2ae4ab55fef74271
Anjali11 <anjali.parikh@mail.utoronto.ca>
2023-04-14 16:18:57 -0400

    make measurements page dynamic

--- frontend/src/user-pages/view-measurements.js
+++ frontend/src/user-pages/view-measurements.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box } from "@mui/material"
 import { WelcomeBanner } from "../components/welcome-banner";
 import { TabPanel } from '../components/account-page-components/tab-panel';
@@ -10,18 +10,30 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useAuth, useDatabase } from '../contexts/auth-context';
+import { onValue, ref, get } from "firebase/database";
 
 
 export const ViewMeasurements = () => {
     // TODO: make the measurements dynamic (connect to backend)
     // weight is in lb, the rest in inches
-    const [measurements, setMeasurements] = useState([
-        ["Height", 64],
-        ["Weight", 130],
-        ["Waist", 25],
-        ["Legs", 28],
-    ]);
-
+    const { user } = useAuth()
+    const db = useDatabase();
+    const [measurements, setMeasurements] = useState([['CHEST'], ['HIP'], ['LEG'], ['SHOULDER'], ['SLEEVE'], ['WAIST']])
+   
+    useEffect(() => {
+        const dbRef = ref(db, '/users/' + user.uid + '/data');
+        onValue(dbRef, (snapshot) => {
+        if (snapshot.exists()){
+            const measure_list = snapshot.val()
+            let i = 0
+            const new_measurements = []
+            Object.entries(measure_list)
+            .map(([key, value]) => new_measurements.push([key.toUpperCase(), value]) )
+            setMeasurements(new_measurements)
+        }
+        })
+    }, [user]) 
 
     return (
         <div>
@@ -31,20 +43,19 @@ export const ViewMeasurements = () => {
             <Box className='main-content'>
                 <TableContainer sx={{ width: 160, boxShadow: "none" }} component={Paper}>
                     <Table aria-label="simple table">
-                        <TableBody>
-                        {measurements.map((measurement) => (
-                            <TableRow
-                                key={measurement}
-                                sx={{ 'td, th': { border: 0 }, 'th': { fontWeight: 1000 } }}>
-                            <TableCell component="th" scope="row" className='measurement-label'>
-                                {measurement[0]}
-                            </TableCell>
-                            <TableCell align="right">
-                                {/* all measurements are in inches, except for weight */}
-                                {`${measurement[1]} ${(measurement[0].localeCompare("Weight") !== 0) ? "in" : "lb"}`}
-                            </TableCell>
-                            </TableRow>
-                        ))}
+                        <TableBody >
+                        {measurements.map((preference) => (
+                                <TableRow
+                                    key={preference}
+                                    sx={{ 'td, th': { border: 0 }, 'th': { fontWeight: 1000 } }}>
+                                <TableCell component="th" scope="row" className='measurement-label'>
+                                    {preference[0]}
+                                </TableCell>
+                                <TableCell align="center">
+                                    {preference[1]} 
+                                </TableCell>
+                                </TableRow>
+                            ))}
                         </TableBody>
                     </Table>
                 </TableContainer>
