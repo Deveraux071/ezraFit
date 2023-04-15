02c70f869611b831ee01d8421af07851e23265c8
Aashna Vasudeva <aashna.vasudeva@mail.utoronto.ca>
2023-04-15 14:08:29 -0400

    removed about us + refactoring

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -5,11 +5,6 @@ import { JointButton } from "./joint-button";
 import { Grid, Link, Typography, Divider, Box } from "@mui/material";
 import { useAuth } from "../contexts/auth-context";
 
-const links = [{
-    title: 'About Us',
-    nav: '/about',
-}]
-
 export const Navigation = ( {loggedIn} ) => {
     const navigate = useNavigate();
     const { logout } = useAuth(); 
@@ -30,17 +25,10 @@ export const Navigation = ( {loggedIn} ) => {
                     <Typography color={theme.colors.black} fontSize='2.25rem' fontWeight={700} sx={{paddingLeft: 2, paddingRight: 2}}>EzraFit</Typography>
                 </Grid>
                 <Divider orientation="vertical" variant="middle" flexItem/>
-                <Grid item xs={5} sx={{p: 2}} display='flex' flexDirection='row'>
+                <Grid item xs={6} sx={{p: 2}} display='flex' flexDirection='row'>
                     <Typography color={theme.colors.black} fontSize='1.75rem' fontWeight={500}>Get your fit</Typography>
                 </Grid>
-                {links.map((link, index) => {
-                    return (
-                        <Grid item key={index} xs={1} display='flex' flexDirection='row' justifyContent='center'>
-                            <Link onClick={() => navigate(link.nav)} sx={{color: theme.colors.pink, fontSize:'1.15rem'}} underline='none'>{link.title}</Link>
-                        </Grid>
-                    )
-                })}
-                <Grid item xs={2} display='flex' flexDirection='row' justifyContent='center' sx={{marginLeft: 3, marginRight: 3}}>
+                <Grid item xs={3} display='flex' flexDirection='row' justifyContent='flex-end' sx={{marginLeft: 3}}>
                     <JointButton info={btns}/>
                 </Grid>
             </Grid>
