565dad5589e67a457eb4ead472953a68f49c6a08
mesropy <lilit.mesropyan@gmail.com>
2023-04-15 07:15:49 -0400

    Refactor to reduce repeated code

--- frontend/src/auth-components/login-form.js
+++ frontend/src/auth-components/login-form.js
@@ -1,12 +1,8 @@
 import * as React from 'react';
-import { PinkFillButton } from '../components/pink-fill-button';
-import { PinkOutlineButton } from '../components/pink-outline-button';
-import { Box, Typography, Link } from '@mui/material';
-import { theme } from '../theme';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../contexts/auth-context";
 import { useState } from 'react';
-import { GridFormItem } from '../styled-grids/grid-form-item';
+import LoginRegisterForm from './login-register-form';
 
 export default function LoginForm( {formWidth} ) {
     const auth = useAuth()
@@ -28,22 +24,12 @@ export default function LoginForm( {formWidth} ) {
         })
     };
 
+    const inputs = [
+        {label: 'Email:', id: 'email', onChange: setEmail},
+        {label: 'Password:', id: 'password', onChange: setPw, type: 'password'},
+    ]
+
     return (
-        <Box sx={{ paddingTop: 3, paddingBottom: 6, display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: theme.colors.white, width: '35%'}}>
-            <Typography fontSize='2.5rem' fontWeight="1000">Welcome! <span style={{fontWeight:'300'}}> - Log In</span> </Typography>
-            <Box sx={{ mt: 1, width: '80%', marginTop: "0px"}} width={formWidth} display='flex' flexDirection='column' justifyContent='center' alignSelf="center">
-                <GridFormItem title='Email:' height="50px" titleSize='1rem' textSize='0.75rem' id='email' onChange={(e) => {setEmail(e.target.value)}}/>
-                <GridFormItem title='Password:' height="50px" titleSize='1rem' textSize='0.75rem' id="password" onChange={(e) => {setPw(e.target.value)}} type="password"/>
-                <Typography textAlign="center" fontSize='1rem' color={theme.colors.red}>{err}</Typography>
-                <Box display='flex' flexDirection='column' justifyContent='top' alignItems='center' sx={{marginTop: 3}}>
-                    <PinkFillButton text='Log In' fontSize='1rem' onClick={(e) => onSubmit(e)} disabled={loading}/>
-                    <Typography marginTop="10px">
-                        Don't have an account? <Link component="button" onClick={() => navigate('/register')}>Sign up here!</Link>
-                    </Typography>
-                    <Typography marginBottom="15px" marginTop="15px" fontSize='1.5rem' fontWeight="1000"> OR </Typography>
-                    <PinkOutlineButton text='Continue as a guest' onClick={() => navigate('/take-image')} fontSize='1rem'/>
-                </Box>
-            </Box>
-        </Box>
+        <LoginRegisterForm formWidth={formWidth} login={true} inputs={inputs} onSubmit={onSubmit} err={err} loading={loading}/>
     );
 }
--- frontend/src/auth-components/register-form.js
+++ frontend/src/auth-components/register-form.js
@@ -1,15 +1,11 @@
 import * as React from 'react';
-import { PinkFillButton } from '../components/pink-fill-button';
-import { PinkOutlineButton } from '../components/pink-outline-button';
-import { Box, Typography, Link } from '@mui/material';
-import { theme } from '../theme';
 import { useNavigate } from 'react-router-dom';
 import { useAuth, useDatabase } from '../contexts/auth-context';
 import { useState } from 'react';
 import { updateProfile } from 'firebase/auth'
 import { getAuth } from 'firebase/auth';
 import { ref, set } from "firebase/database";
-import { GridFormItem } from '../styled-grids/grid-form-item';
+import LoginRegisterForm from './login-register-form';
 
 export default function RegisterForm( {formWidth} ) {
     const db = useDatabase();
@@ -42,24 +38,14 @@ export default function RegisterForm( {formWidth} ) {
         })
     };
 
+    const inputs = [
+        {label: 'First Name:', id: 'first_name', onChange: setFirst},
+        {label: 'Last Name:', id: 'last_name', onChange: setLast},
+        {label: 'Email:', id: 'email', onChange: setEmail},
+        {label: 'Password:', id: 'password', onChange: setPw, type:"password"},
+    ]
+
     return (
-        <Box sx={{ paddingTop: 3, paddingBottom: 6, display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: theme.colors.white, width: '40%'}}>
-            <Typography fontSize='2.5rem' fontWeight="1000">Welcome! <span style={{fontWeight:'300'}}> - Sign Up</span> </Typography>
-            <Box sx={{ mt: 1, width: '80%', marginTop: "0px"}} width={formWidth} display='flex' flexDirection='column' justifyContent='center' alignSelf="center">
-            <GridFormItem title='First Name:' height="50px" titleSize='1rem' textSize='0.75rem' id='first_name' onChange={(e) => {setFirst(e.target.value)}}/>
-            <GridFormItem title='Last Name:' height="50px" titleSize='1rem' textSize='0.75rem' id='last_name' onChange={(e) => {setLast(e.target.value)}}/>
-            <GridFormItem title='Email:' height="50px" titleSize='1rem' textSize='0.75rem' id='email' onChange={(e) => {setEmail(e.target.value)}}/>
-            <GridFormItem title='Password:' height="50px" titleSize='1rem' textSize='0.75rem' id='password' onChange={(e) => {setPw(e.target.value)}} type="password"/>
-                <Typography textAlign="center" fontSize='1rem' color={theme.colors.red}>{err}</Typography>
-                <Box display='flex' flexDirection='column' justifyContent='top' alignItems='center' sx={{marginTop: 3}}>
-                    <PinkFillButton text='Sign Up' fontSize='1rem' onClick={(e) => onSubmit(e)} />
-                    <Typography marginTop="10px">
-                        Already have an account? <Link component="button" onClick={() => navigate('/login')}>Log in here!</Link>
-                    </Typography>
-                    <Typography marginBottom="15px" marginTop="15px" fontSize='1.5rem' fontWeight="1000"> OR </Typography>
-                    <PinkOutlineButton text='Continue as a guest' onClick={() => navigate('/take-image')} fontSize='1rem'/>
-                </Box>
-            </Box>
-        </Box>
+        <LoginRegisterForm formWidth={formWidth} login={false} inputs={inputs} onSubmit={onSubmit} err={err}/>
     );
 }
