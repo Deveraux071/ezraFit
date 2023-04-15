6542657d26fe3dcb80885cb58aea13741f866e5c
mesropy <lilit.mesropyan@gmail.com>
2023-04-15 13:48:24 -0400

    Update camera page instructions
    To make them more descriptive and accessible

--- frontend/src/components/instructions.js
+++ frontend/src/components/instructions.js
@@ -9,20 +9,23 @@ export const Instructions = ( {imageType} ) => {
         info = 'showing your full body.'
     }
 
+    const instructions = imageType === 'check' ? ["Get out 9x6 inch black and white checkboard image, where the side length of each square is 2.84cm long."] : []
+    instructions.push(... [
+        `Set the desired timer length, in seconds, in the "Timer" dropdown below.`,
+        `Press the pink "START" button to start the countdown.`,
+    ])
+    instructions.push(imageType === 'check' ? `Hold up the checkboard to your chest.'` : `Stand in the silhouette`)
+
     return (
         <Box display='flex' flexDirection='column'>
-            <Typography fontSize='2rem' sx={{marginTop: 5, marginBottom: 3}} justifyContent='center'>Instructions</Typography>
+            <Typography fontSize='2rem' sx={{marginTop: 5, marginBottom: 2}} justifyContent='center'>Instructions</Typography>
             <Box sx={{m: 1}}>
-                <Typography fontSize='1.2rem'>
-                    1. Set the timer to your desired length.
-                </Typography>
-                <Typography fontSize='1.2rem'>
-                    2. Press start button.
-                </Typography>
-                <Typography fontSize='1.2rem'>
-                    {imageType === 'check' ? '3. Hold up the checkboard to your chest.' : '3. Stand in the silhouette ' + info}
-                </Typography>
-                <Typography fontSize='1.2rem'>
+                {instructions.map((instruction, i) => 
+                    <Typography fontSize='1.2rem' key={instruction}>
+                        {`${i+1}. ${instruction}`}
+                    </Typography>
+                )}
+                <Typography fontSize='1.2rem' marginTop="5px" marginBottom="10px" fontWeight="600">
                     EzraFit will automatically take a picture when the timer runs out!
                 </Typography>
             </Box>
