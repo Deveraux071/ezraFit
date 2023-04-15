e965a15719d548c5fc6dd3aa5964032387b26008
Aashna Vasudeva <aashna.vasudeva@mail.utoronto.ca>
2023-04-15 15:38:13 -0400

    reduce lines calculating

--- frontend/src/camera-pages/calculating.js
+++ frontend/src/camera-pages/calculating.js
@@ -8,29 +8,24 @@ import { PinkFillButton } from "../components/pink-fill-button";
 
 export default function Calculating() {
     const location = useLocation()
-    let points = location.state !== null ? location.state.points : {};
+    let points = location.state?.points || {};
     const { user } = useAuth()
     const navigate = useNavigate();
     const [errMsg, setErrMsg] = useState('')
+
     useEffect(() => {
         const check = localStorage.getItem('check') !== null ? localStorage.getItem('check').split(',')[1] : ''
         fetch('http://localhost:5000/get-measurements', {
             method: 'POST',
-            headers: { 
-                'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json',
-            },
+            headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'},
             body: JSON.stringify({checkboardImg: check, points: points, company: 'zara', user: user ? user.uid : ''})
         }).then(response => response.json()).then(data => {
                 navigate('/calculated', {state: {size: data}})}).catch(() => setErrMsg('Your checkboard was not detected. Please use clearer images and try again.'))}, [])
     
     return (
         <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' sx={{height: '100vh'}}>
-            <Helmet>
-                <title>Get Measurements - Calculating Measurements | EzraFit</title>
-            </Helmet>
-            <Typography fontSize='2rem' fontWeight={800}>
-                Please stand by while we calculate your measurements! 
-            </Typography>
+            <Helmet><title>Get Measurements - Calculating Measurements | EzraFit</title></Helmet>
+            <Typography fontSize='2rem' fontWeight={800}>Please stand by while we calculate your measurements!</Typography>
             <Typography fontSize='1.5rem' fontWeight={700} color={theme.colors.gray}>Loading... </Typography>
             {errMsg ? <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
                 <Typography fontSize='1.25rem' fontWeight={600} color={theme.colors.red}>{errMsg}</Typography>
