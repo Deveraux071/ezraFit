8302a10dee21392f83dc2caa229aae7a0b634362
mesropy <lilit.mesropyan@gmail.com>
2023-04-15 19:18:24 -0400

    Change import to fix CI

--- backend/body_measurement/code2.py
+++ backend/body_measurement/code2.py
@@ -1,7 +1,7 @@
 import argparse
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import argparse
 import math
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..', '')))
