a1c31346637e5289addad696544d429273600fe6
Aashna Vasudeva <aashna.vasudeva@mail.utoronto.ca>
2023-04-15 11:18:50 -0400

    testing 2

--- frontend/src/camera-pages/calculated.js
+++ frontend/src/camera-pages/calculated.js
@@ -3,10 +3,12 @@ import { PinkOutlineButton } from "../components/pink-outline-button"
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from 'react-router-dom';
 import { CalculatedPopup } from "../components/calculated-popup";
+import { useAuth } from '../contexts/auth-context';
 
 export default function Calculated() {
     const navigate = useNavigate()
     const location = useLocation()
+    const { user } = useAuth()
 
     useEffect(() => {
         localStorage.removeItem('check')
@@ -23,11 +25,6 @@ export default function Calculated() {
         setShowSize(true);
     }
 
-    const navigateTo = (e, url) => {
-        e.preventDefault();
-        navigate(url);
-
-    }
     return (
         <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' sx={{height: '100vh'}}>
             <Typography fontSize='2rem' fontWeight={800}>
@@ -37,8 +34,8 @@ export default function Calculated() {
             <Typography fontSize='1.5rem' fontWeight={700}>
                 Do more with EzraFit!
             </Typography>
-            <PinkOutlineButton onClick={(e) => navigateTo(e, '/style')} text='Get Style Recommendations' fontSize='1.75rem'/>
-            <PinkOutlineButton onClick={(e) => navigateTo(e, '/register')} text='Create Account' fontSize='1.75rem'/>
+            <PinkOutlineButton onClick={() => navigate('/style')} text='Get Style Recommendations' fontSize='1.75rem'/>
+            {user ? <PinkOutlineButton onClick={() => navigate('/account')} text='View Account' fontSize='1.75rem'/> : <PinkOutlineButton onClick={() => navigate('/register')} text='Create Account' fontSize='1.75rem'/>}
             <CalculatedPopup onCancel={() => setShowSize(false)} open={showSize} size={size}/>
         </Box>
     )
--- frontend/src/tests/edit-account2.test.js
+++ frontend/src/tests/edit-account2.test.js
@@ -5,7 +5,7 @@ import { AuthProvider } from '../contexts/auth-context';
 import { EditAccount } from '../user-pages/edit-account';
 import { ViewAccount } from '../user-pages/view-account';
 
-test('renders EditAccount', () => {
+test('redirects to Account page on Cancel', async () => {
     render(
         <Router>
             <AuthProvider>
@@ -16,22 +16,6 @@ test('renders EditAccount', () => {
             </AuthProvider>
         </Router>
     )
-    expect(screen.getByText('Edit Account')).toBeInTheDocument()
-    expect(screen.getByText('Delete My Account')).toBeInTheDocument()
-})
-
-test('redirects to Account page on Save', async () => {
-    render(
-        <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route index element={<EditAccount/>}/>
-                    <Route path='/account' element={<ViewAccount/>}/>
-                </Routes>
-            </AuthProvider>
-        </Router>
-    )
-    screen.debug(undefined, 1000000)
-    fireEvent.click(screen.getByText('Save Changes'))
+    fireEvent.click(screen.getByText('Cancel'))
     await waitFor(() => {screen.getByText('View Account')})
 })
